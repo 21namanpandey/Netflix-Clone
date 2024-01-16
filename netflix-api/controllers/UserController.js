fa4af56557b9650a9b5ad786f41654aef9a088aa
Naman Pandey <2492004namanpandey@gmail.com>
2024-01-16 15:07:29 +0530

    Node.js api add movie to MongoDB

--- netflix-api/controllers/UserController.js
+++ netflix-api/controllers/UserController.js
@@ -1,3 +1,25 @@
 const User = require("../models/UserModel");
 
-module.exports.addToLikedMovies = async(rq,res) => {};
+module.exports.addToLikedMovies = async (rq, res) => {
+    try {
+        const { email, data } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            const { likedMovies } = user;
+            const movieAlreadyLiked = likedMovies.find(({ id }) => (id = data.id));
+            if (!movieAlreadyLiked) {
+                await User.findByIdAndUpdate(
+                    user._id,
+                    {
+                        likedMovies: [...user.likedMovies, , data],
+                    },
+                    { new: true }
+                );
+            } else return res.json({ msg: "Movie already added to liked list." });
+        } else await User.create({ email, likedMovies: [data] })
+        return res.json({ msg: "Movie added Successfully." });
+
+    } catch (error) {
+        return res.json({ msg: "Error adding movie." })
+    }
+};

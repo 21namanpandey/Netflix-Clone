ffe496805a4b5ad319f6b2f6692ea38755d57aa2
Naman Pandey <2492004namanpandey@gmail.com>
2024-01-17 13:27:49 +0530

    using add movie api with react

--- netflix-api/controllers/UserController.js
+++ netflix-api/controllers/UserController.js
@@ -1,17 +1,17 @@
 const User = require("../models/UserModel");
 
-module.exports.addToLikedMovies = async (rq, res) => {
+module.exports.addToLikedMovies = async (req, res) => {
     try {
         const { email, data } = req.body;
         const user = await User.findOne({ email });
         if (user) {
             const { likedMovies } = user;
-            const movieAlreadyLiked = likedMovies.find(({ id }) => (id = data.id));
+            const movieAlreadyLiked = likedMovies.find(({ id }) => (id === data.id));
             if (!movieAlreadyLiked) {
                 await User.findByIdAndUpdate(
                     user._id,
                     {
-                        likedMovies: [...user.likedMovies, , data],
+                        likedMovies: [...user.likedMovies, data],
                     },
                     { new: true }
                 );
--- netflix-ui/src/components/Card.jsx
+++ netflix-ui/src/components/Card.jsx
@@ -7,12 +7,31 @@ import { RiThumbUpFill, RiThumbDownFill } from "react-icons/ri"
 import { BsCheck } from "react-icons/bs"
 import { AiOutlinePlus } from "react-icons/ai"
 import { BiChevronDown } from "react-icons/bi"
+import { onAuthStateChanged } from 'firebase/auth';
+import { firebaseAuth } from '../utils/firebase-config';
+import axios from 'axios';
 
 
 const Card = React.memo(({ movieData, isLiked = false }) => {
 
     const [isHovered, setIsHovered] = useState(false);
+    const [email, setEmail] = useState(undefined);
     const navigate = useNavigate();
+
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+        if (currentUser) setEmail(currentUser.email);
+        else navigate("/login");
+    });
+
+    const addToList = async () => {
+        try {
+            await axios.post("http://127.0.0.1:5000/api/user/add", { email, data: movieData })
+            
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <Container onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
@@ -39,7 +58,7 @@ const Card = React.memo(({ movieData, isLiked = false }) => {
                                         isLiked ? (
                                             <BsCheck title="Remove From List" />
                                         ) : (
-                                            <AiOutlinePlus title='Add to my list' />
+                                            <AiOutlinePlus title='Add to my list' onClick={addToList} />
                                         )
                                     }
                                 </div>
